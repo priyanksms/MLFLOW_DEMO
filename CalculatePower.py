438e4e52de44b02a2f8387a70eb6f507b59a3886
Jain <priyank.jain@sms-group.com>
2023-04-02 11:06:02 +0530

    added First Action yml

--- CalculatePower.py
+++ CalculatePower.py
@@ -7,6 +7,7 @@ def calculate_power(x,n):
 if __name__=='__main__':
     with mlflow.start_run():
         x,n = 2,5
+        print(7)
         y = calculate_power(x,n)
         mlflow.log_param("x",x)
         mlflow.log_param("n",n)
