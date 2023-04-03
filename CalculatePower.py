489e964e42566c3cff0b824a557d80ab86195840
Jain <priyank.jain@sms-group.com>
2023-04-03 14:04:54 +0530

    some changes

--- CalculatePower.py
+++ CalculatePower.py
@@ -8,7 +8,7 @@ if __name__=='__main__':
     with mlflow.start_run():
         x,n = 2,5
         print(7)
-        y = calculate_power(x,n)
+        y = calculate_power(x,n )
         mlflow.log_param("x",x)
         mlflow.log_param("n",n)
         mlflow.log_metric("y",y)
